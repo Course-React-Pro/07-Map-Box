cae62dd56a4c8acbc0179862e24a42f78419360a
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-03-28 11:01:38 -0500

    select place

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,29 +1,44 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PlacesContext } from '../context/places/PlacesContext';
+import { Feature } from '../interfaces/searchResponse.interface';
+import { MapContext } from '../context/map/MapContext';
 
 const SearchResults = () => {
 
   const { places } = useContext(PlacesContext)
+  const { map } = useContext(MapContext)
+  const [activePlace, setActivePlace] = useState('')
+
+  const onPlaceClick = (place: Feature) => {
+    setActivePlace(place.id)
+    const [lng, lat] = place.center;
+    map?.flyTo({
+      zoom: 14,
+      duration: 500,
+      animate: true,
+      center: [lng, lat],
+    })
+  }
 
   if ( !places.length ) {
     return <></>
   }
-
-
   return (
     <ul className='list-group mt-3'>
       {
         places?.map( item => (
-          <li className='list-group-item list-group-item-action'>
+          <li 
+            className={`list-group-item list-group-item-action pointer ${activePlace === item.id ? 'active' : ''}`}
+            onClick={() => onPlaceClick(item)}
+          >
             <h6>{item.place_name_es}</h6>
-            <p 
-              className="text-muted"
+            <p
               style={{ fontSize: '0.8rem' }}
             >
               {item.place_name}
             </p>
             <button 
-              className="btn btn-outline-primary btn-sm"
+              className="btn btn-secondary btn-sm"
             >
               Direciones
             </button>
